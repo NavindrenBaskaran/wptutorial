0b7b7ebbdf7aabdffb386dad5cb341751c42fdfa
Navindren Baskaran <navin25@hotmail.com>
2017-03-04 23:56:34 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+import Button from './Components/Button';
+
+const button = new Button('google.com');
+button.render('a');
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src',
+  output: {
+    path: 'builds',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders : [
+        {
+          test: /\.js/,
+          loader: 'babel-loader',
+          include: __dirname + '/src',
+        },
+        {
+          test: /\.scss/,
+          loaders : ['style-loader','css-loader','sass-loader'],
+        },
+        {
+          test: /\.html/,
+          loader: 'html-loader',
+        }
+    ],
+  }
+
+}
