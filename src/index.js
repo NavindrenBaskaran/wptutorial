3e1567c6873f6b3d6250927c6c66e1edd4402b0a
Navindren Baskaran <navin25@hotmail.com>
2017-03-05 00:46:46 +0800

    Add code for code splitting'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import $ from 'jquery';
 import Button from './Components/Button';
 
 window.onload = function(){
-  const button = new Button('google.com');
-  button.render('.button-area');
+
+
+if(document.querySelectorAll('.button-area').length){
+    require.ensure([], () => {
+      const Button = require('./Components/Button').default;
+      const button = new Button('google.com');
+
+      button.render('.button-area');
+    });
+}
 };
